79a5bf796fdb67a6ef2ecfc808fe7f8d9856fe59
kappassov <abayk0708@gmail.com>
2022-07-05 16:00:06 +0600

    added svg icon

--- src/components/world/index.jsx
+++ src/components/world/index.jsx
@@ -1,12 +1,17 @@
 import { useState, useEffect, useContext } from "react";
 import Globe from "react-globe.gl";
 import { CountryContext } from "../../context";
-
+import { markerSvg } from "../../assets/camera";
+// import Lightbox from "react-18-image-lightbox";
+// import "react-18-image-lightbox/style.css";
 const World = () => {
   const [countriesJson, setCountriesJson] = useState({ features: [] });
-  const [hoverD, setHoverD] = useState();
+  //const [hoverD, setHoverD] = useState();
   const { countries } = useContext(CountryContext);
   //const [selected, setSelected] = useState([]);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [imgIndex, setImgIndex] = useState(0);
 
   // console.log("world", countries);
   useEffect(() => {
@@ -27,15 +32,55 @@ TODO:
   const selected = [];
   selected.push(countries);
 
+  // Gen random data
+  const N = 30;
+  // const gData = [...Array(N).keys()].map(() => ({
+  //   lat: (Math.random() - 0.5) * 180,
+  //   lng: (Math.random() - 0.5) * 360,
+  //   size: 7 + Math.random() * 30,
+  //   color: ["red", "white", "blue", "green"][Math.round(Math.random() * 3)],
+  // }));
+
+  const gData = [
+    {
+      lat: 60,
+      lng: 90,
+      size: 30,
+      color: "red",
+    },
+  ];
+
+  const images = [
+    {
+      title: "Travel 1",
+      caption: "Caption 1",
+      url: "//unsplash.com/photos/xr-y6Ruw7K8",
+    },
+  ];
+
+  // {
+  //   isOpen && (
+  //     <Lightbox
+  //       imageTitle={images[imgIndex].title}
+  //       imageCaption={images[imgIndex].caption}
+  //       mainSrc={images[imgIndex].url}
+  //       nextSrc={images[(imgIndex + 1) % images.length].url}
+  //       prevSrc={images[(imgIndex + images.length - 1) % images.length].url}
+  //       onCloseRequest={() => setIsOpen(false)}
+  //       onMovePrevRequest={() =>
+  //         setImgIndex((imgIndex + images.length - 1) % images.length)
+  //       }
+  //       onMoveNextRequest={() => setImgIndex((imgIndex + 1) % images.length)}
+  //     />
+  //   );
+  // }
+
   return (
     <Globe
       globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-      //   globeImageUrl="//../../assets/8k_earth_daymap.jpg"
       backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
       lineHoverPrecision={0}
-      polygonsData={countriesJson.features.filter(
-        (d) => d.properties.ISO_A2 !== "AQ"
-      )}
+      polygonsData={countriesJson.features}
       polygonAltitude={(d) =>
         selected[0].includes(d.properties.ISO_A2) ? 0.12 : 0.01
       }
@@ -47,8 +92,23 @@ TODO:
       polygonLabel={({ properties: d }) => `
         <b>${d.ADMIN} (${d.ISO_A2})</b>
       `}
-      onPolygonHover={setHoverD}
+      //onPolygonHover={setHoverD}
       polygonsTransitionDuration={300}
+      htmlElementsData={gData}
+      htmlElement={(d) => {
+        const el = document.createElement("div");
+        el.innerHTML = markerSvg;
+        el.style.color = d.color;
+        el.style.width = `${d.size}px`;
+
+        el.style["pointer-events"] = "auto";
+        el.style.cursor = "pointer";
+        el.onclick = () => {
+          setIsOpen(true);
+          console.log(isOpen);
+        };
+        return el;
+      }}
     />
   );
 };
